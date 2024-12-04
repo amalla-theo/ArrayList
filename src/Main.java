4735f0086475c92704c8a0e5693b8f2aabe2505a
tamalla <theo.amalla@lyceeastier.com>
2024-12-04 14:35:04 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        char c = 'z';
+        int valeur = 25;
+        String s = "Chaine de caractères";
+        double d = 32.25;
+        StringBuilder sb = new StringBuilder();
+        ArrayList maListe = new ArrayList();
+
+        maListe.add(c);
+        maListe.add(valeur);
+        maListe.add(s);
+        maListe.add(d);
+        maListe.add(sb);
+
+        System.out.println(maListe);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(maListe.get(i).getClass());
+        }
+
     }
 }
